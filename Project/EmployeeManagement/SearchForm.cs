ea87552e8e70ad408007930e385fd54dd655164a
ishikawa-tatsuya <ishikawa-tatsuya@codeer.co.jp>
2015-01-13 23:46:12 +0900

    テスト対象追加

---
+++ Project/EmployeeManagement/SearchForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace EmployeeManagement
+{
+    public partial class SearchForm : Form
+    {
+        IEnumerable<EmployeeData> _items;
+
+        public SearchForm(IEnumerable<EmployeeData> items)
+        {
+            InitializeComponent();
+            _items = items;
+        }
+
+        private void ButtonExecuteClick(object sender, EventArgs ev)
+        {
+            _listBoxEmployee.Items.Clear();
+            _listBoxEmployee.Items.AddRange(_items.Where(e => e.Address.IndexOf(_textBoxSearch.Text) != -1).ToArray());
+        }
+    }
+}
